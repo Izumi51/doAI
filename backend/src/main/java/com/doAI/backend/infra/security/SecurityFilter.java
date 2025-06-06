d5df331943b23da707023f96b98fa64967081671
Izumi <joaoizumi27@gmail.com>
2025-06-06 20:10:07 -0300

    update: changes on securityFilter verifications

--- backend/src/main/java/com/doAI/backend/infra/security/SecurityFilter.java
+++ backend/src/main/java/com/doAI/backend/infra/security/SecurityFilter.java
@@ -26,14 +26,21 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        var login = tokenService.validateToken(token);
-
-        if(login != null) {
-            User user = userRepository.findByEmail(login).orElseThrow(() -> new RuntimeException("User Not Found"));
-            var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            var token = this.recoverToken(request);
+            
+            if (token != null) {
+                var login = tokenService.validateToken(token);
+                
+                if (login != null) {
+                    User user = userRepository.findByEmail(login).orElseThrow(() -> new RuntimeException("User Not Found"));
+                    var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }catch (Exception e) {
+            SecurityContextHolder.clearContext();
         }
         filterChain.doFilter(request, response);
     }
