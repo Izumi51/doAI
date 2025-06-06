7faabd7ed9aeeb5fef2e5e41e1916bff915256af
Izumi <joaoizumi27@gmail.com>
2025-06-06 19:06:00 -0300

    update: adding Annotation @Repository for the repository interfaces

--- backend/src/main/java/com/doAI/backend/repository/UserRepository.java
+++ backend/src/main/java/com/doAI/backend/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.doAI.backend.repository;
 
 import com.doAI.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 }
